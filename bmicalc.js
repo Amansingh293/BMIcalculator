b618151d33e1c10d325f609452e65b52099059cf
Amansingh293 <106438123+Amansingh293@users.noreply.github.com>
2022-07-16 03:04:35 +0530

    Update bmicalc.js

--- bmicalc.js
+++ bmicalc.js
@@ -32,6 +32,6 @@ app.post("/", function(req, res) {
   }
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("server has started on 3000port");
 });
